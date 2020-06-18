ae00d14ebfd3e566f5f2bbf322d31c5027d0f9be
Aishwarya <aishwaryasagavakar@gmail.com>
2020-06-18 10:23:36 +0530

    COnverter

---
+++ app/src/main/java/com/example/globalconverter/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.globalconverter
+
+import android.icu.text.Transliterator
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_prssure.*
+import java.util.*
+
+
+class MainActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/globalconverter/main.kt
@@ -0,0 +1,51 @@
+package com.example.globalconverter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class main : Fragment() ,View.OnClickListener {
+
+    var navController: NavController? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<ImageView>(R.id.imageView).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.imageView2).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.imageView3).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.imageView4).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.imageView6).setOnClickListener(this)
+        view.findViewById<ImageView>(R.id.imageView11).setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.imageView -> navController!!.navigate(R.id.action_main_to_money)
+            R.id.imageView2 -> navController!!.navigate(R.id.action_main_to_temp)
+            R.id.imageView3 -> navController!!.navigate(R.id.action_main_to_weight)
+            R.id.imageView4 -> navController!!.navigate(R.id.action_main_to_distance)
+            R.id.imageView6 -> navController!!.navigate(R.id.action_main_to_scale)
+            R.id.imageView11 -> navController!!.navigate(R.id.action_main_to_prssure)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
+
+
+
+
